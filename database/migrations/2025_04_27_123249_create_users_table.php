e98e8e552fb91a6c5d77b09737f13bf23817a060
Dale Guantia <dalefguantia@gmail.com>
2025-09-08 08:01:31 +0800

    09/08/2025

--- database/migrations/2025_04_27_123249_create_users_table.php
+++ database/migrations/2025_04_27_123249_create_users_table.php
@@ -23,7 +23,7 @@ return new class extends Migration
             $table->boolean('is_active')->default(true);
             $table->integer('role')->default(User::DEFAULT_ROLE);
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 255);
             $table->integer('resolved_tickets_count')->default(0);
             $table->rememberToken();
             $table->timestamps();
