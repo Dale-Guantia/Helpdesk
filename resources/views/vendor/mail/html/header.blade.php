af69a8dc6c421c35ac907587a9cb06833fcaf246
Dale Guantia <dalefguantia@gmail.com>
2025-12-03 11:27:20 +0800

    12/03/2025

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -1,12 +1,8 @@
 @props(['url'])
 <tr>
-<td class="header">
-<a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="{{ asset('images/PrimaryLogo.png') }}" alt="{{ config('app.name') }} logo" style="width: 100%; height: 100px; max-width: 100%; max-height: 100px;">
-@else
-{!! $slot !!}
-@endif
-</a>
-</td>
+    <td class="header">
+        <a href="{{ $url }}" style="display: inline-block;">
+            <img src="{{ asset('images/emp-care-logo-2.png') }}" alt="{{ config('app.name') }} logo" style="width: auto; height: 100px; max-height: 100px;">
+        </a>
+    </td>
 </tr>
